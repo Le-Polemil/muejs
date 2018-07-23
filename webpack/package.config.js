2282973672047ebde9b517ef6a382a649ff5f223
pemoreau <pemoreau@artprice.com>
2018-07-23 11:47:32 +0200

    package finally done ?

--- webpack/package.config.js
+++ webpack/package.config.js
@@ -29,12 +29,7 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ['env']
-                    }
-                }
+                use: "babel-loader"
             },
             {
                 test: /\.styl$/,
