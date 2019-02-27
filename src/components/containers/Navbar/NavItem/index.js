61037ea918f2ad685f7db0211aa0b4bb2074d55b
pemoreau <pemoreau@artprice.com>
2019-02-27 16:12:46 +0100

    Adapt NavItems to React router

--- src/components/containers/Navbar/NavItem/index.js
+++ src/components/containers/Navbar/NavItem/index.js
@@ -4,40 +4,40 @@ import gridify from '../../../../hoc/gridify';
 
 
 
-const Logo = ({ children, imgSrc, route, ...otherProps }) => (
-    <a {...otherProps} href={ route || '/' }>
+const Logo = ({ children, imgSrc, ...otherProps }) => (
+    <span {...otherProps}>
         { imgSrc ? <img src={imgSrc} alt={children}/> : children }
-    </a>
+    </span>
 );
 export const NavLogo = gridify(Logo, { componentName: 'NavLogo' });
 
 
 
 
-const Brand = ({ children, route, ...otherProps }) => (
-    <a {...otherProps} href={ route || '/' }>{ children }</a>
+const Brand = ({ children, ...otherProps }) => (
+    <span {...otherProps}>{ children }</span>
 );
 export const NavBrand = gridify(Brand, { componentName: 'NavBrand' });
 
 
 
 
-const Label = ({ label, route, children, ...otherProps }) => (
-    <a {...otherProps} href={ route }>{ label || children }</a>
+const Label = ({ label, children, ...otherProps }) => (
+    <span {...otherProps}>{ label || children }</span>
 );
 export const NavLabel = gridify(Label, { componentName: 'NavLabel' });
 
 
 
 
-const Icon = ({ icon, route, size, svg, onClick, ...otherProps }) => {
+const Icon = ({ icon, size, svg, onClick, ...otherProps }) => {
     const iconClassNames = ['material-icons', svg ? icon : '', size].filter(e => !!e).join(' ');
     return (
-        <a {...otherProps} href={ route } onClick={onClick}>
+        <span {...otherProps} onClick={onClick}>
             <i className={iconClassNames}>
                 { svg ? <svg viewBox="0 0 512 512" className="svg-container"><path className="svg-path" /></svg> : icon }
             </i>
-        </a>
+        </span>
     );
 };
 export const NavIcon = gridify(Icon, { componentName: 'NavIcon' });
