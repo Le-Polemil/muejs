120083aee5080acf7c45581913a194ec01facf94
pemoreau <pemoreau@artprice.com>
2019-05-06 16:10:47 +0200

    Button & Form

--- src/components/containers/Form/index.js
+++ src/components/containers/Form/index.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import deepEqual from 'lodash.isequal';
+import React, { useReducer } from 'react';
+import produce  from 'immer';
+import set from 'lodash.set';
 import { Grid } from '../Grid';
 import gridify from '../../../hoc/gridify';
 
 import './index.styl';
 
 
+const UngridifiedForm = ({ onSubmit = () => undefined, children, columnsTemplate, rowsTemplate, gap, rowGap, colGap, gridClassName, ...otherProps }) => {
 
-const UngridifiedForm = ({ children, columnsTemplate, rowsTemplate, gap, rowGap, colGap, gridClassName, ...otherProps }) => (
-  <form {...otherProps}>
-    <Grid columnsTemplate={columnsTemplate} rowsTemplate={rowsTemplate} gap={gap} rowGap={rowGap} colGap={colGap} className={gridClassName}>
-      { children }
-    </Grid>
-  </form>
-);
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        onSubmit(e, new FormData(e.currentTarget));
+      }}
+      {...otherProps}
+    >
+      <Grid columnsTemplate={columnsTemplate} rowsTemplate={rowsTemplate} gap={gap} rowGap={rowGap} colGap={colGap} className={gridClassName}>
+        { children }
+      </Grid>
+    </form>
+  );
+}
 
 
 export const Form = gridify(UngridifiedForm, { componentName: 'form' });
