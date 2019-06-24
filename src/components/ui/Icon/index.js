994851e7fe419d309656356278e36ffbddab4eda
Cobelt <paul_emile@hotmail.fr>
2019-06-24 09:03:26 +0200

    Icon upgrade

--- src/components/ui/Icon/index.js
+++ src/components/ui/Icon/index.js
@@ -6,11 +6,15 @@ import { camelToSnake } from "../../../filters/stringFormat";
 
 import './index.styl';
 
-const UngridifiedIcon = ({ children, className = '', size = '', icon, href, ...otherProps }) => {
-    const classNames = ['material-icons', size, className].filter(e => !!e).join(' ');
+const UngridifiedIcon = ({ children, className = '', size = '', svg, icon, href, iconType = 'round', ...otherProps }) => {
+    const iconFormatted = camelToSnake(icon || children);
+    const materialClass = `material-icons${['outlined', 'two-tone', 'round', 'sharp'].includes(iconType) ? `-${iconType}` : ''}`;
+    const classNames = [materialClass,  svg ? iconFormatted : '', size, className].filter(e => !!e).join(' ');
     // if(href) {}
     return (
-        <i className={classNames} {...otherProps}>{ camelToSnake(icon || children) }</i>
+      <i className={classNames} {...otherProps}>
+          { svg ? <svg viewBox="0 0 512 512" className="svg-container"><path className="svg-path" /></svg> : iconFormatted }
+      </i>
     );
 }
 
