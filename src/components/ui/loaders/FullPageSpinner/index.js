548cc88f5e0a61abf347e2856aaf62991086faa1
Cobelt <paul_emile@hotmail.fr>
2019-08-14 22:47:51 +0200

    typo

--- src/components/ui/loaders/FullPageSpinner/index.js
+++ src/components/ui/loaders/FullPageSpinner/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Grid from '../../../containers/Grid';
-import Spinner from './CircularProgress';
+import Spinner from '../CircularProgress';
 
 const FullPageSpinner = (props) => (
   <Grid rowsTemplate="100vh" columnsTemplate="100vw" className="align-center justify-center" {...props}>
