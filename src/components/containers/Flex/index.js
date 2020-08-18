167dd2d12b1ca3cce42f5708f60e4e1468c3b5f3
Paul-Emile MOREAU <paul-emile.moreau@soluti.fr>
2020-08-18 16:10:07 +0200

    remove all components

--- src/components/containers/Flex/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Flex({ children, className, ...otherProps }) {
-  return (
-      <div className={`flex ${className || ''}`} {...otherProps}>
-        { children }
-      </div>
-  );
-}
--- src/hooks/gridify/index.js
+++ src/hooks/gridify/index.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import get from 'lodash.get';
+import React, { useState } from 'react'
+import get from 'lodash.get'
 
-import { camelToKebab } from '../../filters/stringFormat';
-import { getCSSVarForDimension } from '../../utils/gridify';
+import { camelToKebab } from '../../filters/stringFormat'
+import { getCSSVarForDimension } from '../../utils/gridify'
 
 export const useGridify = ({
     col: defaultCol,
@@ -10,50 +10,79 @@ export const useGridify = ({
     width,
     height,
 
-    className, style,
+    className,
+    style,
     position,
-    justify, align,
+    justify,
+    align,
 
-    show, hide,
+    show,
+    hide,
 
     shouldTransmitProps,
-    selfRowTemplate, selfColTemplate,
-    componentName,
-  }) => {
+    selfRowTemplate,
+    selfColTemplate,
+    componentName
+}) => {
+    if ((show !== undefined && !show) || hide) return null
 
-    if (show !== undefined && !show || hide) return null;
+    const isFixed = [position].includes('fixed') ? 'fixed' : ''
+    const col = isFixed ? 0 : defaultCol
+    const row = isFixed ? 0 : defaultRow
 
-    const isFixed = [position].includes('fixed') ? 'fixed' : '';
-    const col = isFixed ? 0 : defaultCol;
-    const row = isFixed ? 0 : defaultRow;
-
-    const type = componentName || Component.displayName || `Gridified${Component.name || 'Component'}`;
+    const type =
+        componentName ||
+        Component.displayName ||
+        `Gridified${Component.name || 'Component'}`
 
     // css variables
-    const styles = { };
+    const styles = {}
     if (parseInt(width, 10) <= 0 && parseInt(height, 10) <= 0) {
-        styles['display'] = 'none';
-    }
-    else {
-      ['xs', 'sm', 'md', 'lg', 'xl', 'xxl', 'xxxl'].forEach(size => {
-        const suffix = size !== 'xs' ? `-${size}` : '';
+        styles['display'] = 'none'
+    } else {
+        ;['xs', 'sm', 'md', 'lg', 'xl', 'xxl', 'xxxl'].forEach(size => {
+            const suffix = size !== 'xs' ? `-${size}` : ''
 
-        styles[`--col${suffix}`] =    getCSSVarForDimension({ dimension: col, size, defaultValue: 'auto' });
-        styles[`--row${suffix}`] =    getCSSVarForDimension({ dimension: row, size, defaultValue: 'auto' });
-        styles[`--width${suffix}`] =  getCSSVarForDimension({ dimension: width, size, defaultValue: 1 });
-        styles[`--height${suffix}`] = getCSSVarForDimension({ dimension: height, size, defaultValue: 1 });
-      });
+            styles[`--col${suffix}`] = getCSSVarForDimension({
+                dimension: col,
+                size,
+                defaultValue: 'auto'
+            })
+            styles[`--row${suffix}`] = getCSSVarForDimension({
+                dimension: row,
+                size,
+                defaultValue: 'auto'
+            })
+            styles[`--width${suffix}`] = getCSSVarForDimension({
+                dimension: width,
+                size,
+                defaultValue: 1
+            })
+            styles[`--height${suffix}`] = getCSSVarForDimension({
+                dimension: height,
+                size,
+                defaultValue: 1
+            })
+        })
     }
 
-    const gridElementProps = shouldTransmitProps ? { col, row, width, height } : {};
+    const gridElementProps = shouldTransmitProps ? { col, row, width, height } : {}
 
     return {
-      hide: ((show !== undefined && !show) || hide),
+        hide: (show !== undefined && !show) || hide,
 
-      className: [camelToKebab(type), className, justify && `justify-${justify}`, align && `align-${align}`, isFixed].filter(e => !!e).join(' '),
-      style: { ...styles, ...style },
-      ...gridElementProps,
+        className: [
+            camelToKebab(type),
+            className,
+            justify && `justify-${justify}`,
+            align && `align-${align}`,
+            isFixed
+        ]
+            .filter(e => !!e)
+            .join(' '),
+        style: { ...styles, ...style },
+        ...gridElementProps
     }
 }
 
-export default useGridify;
+export default useGridify
